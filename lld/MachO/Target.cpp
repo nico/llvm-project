4ce174b356e4b539aecdc68baf9867c9898ed12c
Nico Weber <nicolasweber@gmx.de>
2019-08-30 09:46:23 -0400

    ignore a few more flags, dummy support for a few more relocation types

--- lld/MachO/Target.cpp
+++ lld/MachO/Target.cpp
@@ -1,6 +1,8 @@
 #include "Target.h"
 #include "llvm/BinaryFormat/MachO.h"
 #include "llvm/Support/Endian.h"
+#include "llvm/Support/raw_ostream.h"
+
 
 using namespace lld;
 using namespace lld::macho;
@@ -23,10 +25,15 @@ X86_64::X86_64() {
 uint64_t X86_64::getImplicitAddend(const uint8_t *Loc, uint8_t Type) const {
   switch (Type) {
   case X86_64_RELOC_BRANCH:
+  case X86_64_RELOC_GOT_LOAD:
   case X86_64_RELOC_SIGNED:
   case X86_64_RELOC_SIGNED_1:
+  case X86_64_RELOC_SIGNED_4:
+  case X86_64_RELOC_UNSIGNED:
+  case X86_64_RELOC_TLV:
     return read32le(Loc);
   default:
+    llvm::outs() << "addend " << (int)Type << "\n";
     assert(0);
   }
 }
